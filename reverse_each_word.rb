c96991cfa0bf62e597f8779029020b0232aeb84d
William Bugenis <wbugenis@gmail.com>
2020-12-15 19:34:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(string)
   words = string.split(" ")
   
-  words.collect! { |word| word.reverse }
+  reversed = words.collect { |word| word.reverse }
   
-  words.join(" ")
+  reversed.join(" ")
   
 end
